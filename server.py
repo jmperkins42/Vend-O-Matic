806de4c0872f51aa852e258ea85615a26c074f66
Jonah Perkins <jonah.m.perkins@gmail.com>
2026-04-18 19:58:25 -0500

    separated vendlogic

--- server.py
+++ server.py
@@ -1,82 +1,73 @@
 from flask import Flask, jsonify, request
+from vendlogic import VendingMachine
 app = Flask(__name__)
-
-# global variables to keep up with vending machine state
-coins = 0
-inventory = [5,5,5]
+vending_machine = VendingMachine()
 
 # endpoint to insert a coin
 @app.route('/', methods=['PUT'])
 def insert_coin():
-    # reference to global variable coins
-    global coins
     # gets the req body as JSON
     body = request.get_json()
     # edge case: req body is missing or has invalid value
-    if not body or body.get('coin') != 1:
+    if not vending_machine.insert_coin(body['coin']):
         return '', 400
-    # add the coin value from the req body to the total coins
-    coins += body['coin']
     # send sucessful response
     response = app.make_response(('', 204))
     # send current coins in custom header "X-Coins"
-    response.headers['X-Coins'] = str(coins)
+    response.headers['X-Coins'] = str(vending_machine.get_coins())
     return response
 
 @app.route('/', methods=['DELETE'])
 def return_coins():
-    # reference to global variable coins
-    global coins
     # send successful response with current coins in custom header "X-Coins"
     response = app.make_response(('', 204))
-    response.headers['X-Coins'] = str(coins)
+    response.headers['X-Coins'] = str(vending_machine.get_coins())
     # reset coins to 0 after returning them to the user
-    coins = 0
+    vending_machine.return_coins()
     return response
 
 @app.route('/inventory', methods=['GET'])
 def get_inventory():
     # send successful response with inventory as JSON array in the response body
-    return jsonify(inventory), 200
+    return jsonify(vending_machine.get_inventory()), 200
 
 @app.route('/inventory/<int:id>', methods=['GET'])
 def get_item_quantity(id):
+    quantity = vending_machine.get_item_quantity(id)
     # edge case: if the id is out of bounds, return a 400 Bad Request response
-    if id < 0 or id >= len(inventory):
+    if quantity == -1:
         return '', 400
     # send successful response with quantity as JSON in the response body
-    return jsonify(inventory[id]), 200
+    return jsonify(quantity), 200
 
 @app.route('/inventory/<int:id>', methods=['PUT'])
 def purchase_item(id):
-    # reference to global variables
-    global coins
     num_vended = 0
+    result = vending_machine.purchase_item(id)
     # edge case: if the id is out of bounds, return a 400 Bad Request response
-    if id < 0 or id >= len(inventory):
+    if result == 'OUT_OF_BOUNDS':
         return '', 400
     # edge case: if the item is out of stock,
     # return a 404 Not Found response with the current number of coins in the "X-Coins" header
-    if inventory[id] <= 0:
+    if result == 'OUT_OF_STOCK':
         response = app.make_response(('', 404))
-        response.headers['X-Coins'] = str(coins)
+        response.headers['X-Coins'] = str(vending_machine.get_coins())
         return response
     # edge case: if the user does not have enough coins to purchase the item,
     # return a 403 Forbidden response with the current number of coins in the "X-Coins" header
-    if coins < 2:
+    if result == 'INSUFFICIENT_COINS':
         response = app.make_response(('', 403))
-        response.headers['X-Coins'] = str(coins)
+        response.headers['X-Coins'] = str(vending_machine.get_coins())
         return response
     # if the purchase is successful, return a 200 OK response 
-    response.headers['X-Coins'] = str(coins-2)
+    response = app.make_response(('', 200))
+    # respond with coins remaining after purchase
+    response.headers['X-Coins'] = str(vending_machine.get_coins())
     # respond with current inventory of purchased item after purchase
-    response.headers['X-Inventory-Remaining'] = str(inventory[id]-1)
-    # update coins to reflect purchase and coins returned to user
-    coins = 0
-    # update inventory to reflect purchase
-    inventory[id] -= 1
+    response.headers['X-Inventory-Remaining'] = str(vending_machine.get_item_quantity(id))
     num_vended += 1
     response.body = {"quantity": num_vended}
+    vending_machine.return_coins()  # return coins after purchase
     return response
     
 # run the Flask app on port 8080    
---
+++ vendlogic.py
@@ -0,0 +1,49 @@
+class VendingMachine:
+    def __init__(self):
+        self.coins = 0
+        self.inventory = [5, 5, 5]  # initial inventory for each item
+
+    def get_coins(self):
+        # return the current number of coins
+        return self.coins
+        
+    def insert_coin(self, coin):
+        # edge case: if the coin is not valid, return False
+        if coin != 1:
+            return False
+        # add the coin value to the total coins
+        self.coins += coin
+        return True
+    
+    def return_coins(self):
+        # return the current coins and reset to 0
+        coins_to_return = self.coins
+        self.coins = 0
+        return coins_to_return
+    
+    def get_inventory(self):
+        # return the current inventory
+        return self.inventory
+    
+    def get_item_quantity(self, id):
+        # edge case: if the id is out of bounds, return -1
+        if id < 0 or id >= len(self.inventory):
+            return -1
+        # return the quantity of the specified item
+        return self.inventory[id]
+
+    def purchase_item(self, id):
+        # edge case: id is out of bounds
+        if id < 0 or id >= len(self.inventory):
+            return 'OUT_OF_BOUNDS'
+        # edge case: item is out of stock
+        if self.inventory[id] <= 0:
+            return 'OUT_OF_STOCK'
+        # edge case: user does not have enough coins
+        if self.coins < 2:
+            return 'INSUFFICIENT_COINS'
+        # process the purchase
+        self.coins -= 2  # cost of the item is 2 coins
+        self.inventory[id] -= 1  # reduce inventory by 1
+        # return 'SUCCESS' to indicate a successful purchase
+        return 'SUCCESS'
